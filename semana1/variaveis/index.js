01ac40c2cea23f6737fc5c516b368de93816cc16
MarthaCristinyAvelarGoncalves <marthacristiny@Eds-Mac-mini.local>
2021-06-14 13:29:48 -0300

    exercicio-desafio-const-soma-multiplicacao

---
+++ semana1/variaveis/index.js
@@ -0,0 +1,38 @@
+//Resposta 1 - console.log(10, 5)
+//Resposta 2 - console.log(20, undefined, 10)
+
+/*Resposta 3 - Sera criado um alert com duas perguntas onde voce deve responder 
+quantas horas você trabalha por dia, e quanto você recebe por dia. As varias das
+perguntas estao sendo representadas po "t" e "p". Eu gostaria de sugerir que as 
+variaveis fossem representadas por 
+var horasDia = 24h
+var recebePorDia = 1200 */
+
+//-----------------TREINANDO--------------
+
+const nome = prompt("Qual é o seu nome?")
+const idade1 = Number (prompt("Qual é a sua idade?"))
+
+
+const nome2 = prompt("Qual é seu nome?")
+const idade2 = Number (prompt ("Qual é a sua idade?"))
+
+const somaIdades = idade1 + idade2
+
+console.log("Soma", somaIdades )
+
+//---------------DESAFIO---------
+
+const numero1 = 2
+const numero2 = 5
+
+const somaDosNumeros = numero1 + numero2
+
+console.log("resultado: ", somaDosNumeros)
+
+const numero3 = 2
+const numero4 = 5
+
+const multiplicacaoDosNumeros = numero3 * numero4
+
+console.log("resultado: ", multiplicacaoDosNumeros)
