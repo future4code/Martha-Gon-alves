9bbef861883ffcdfd2445f83984bbe4fb42aa9db
MarthaCristinyAvelarGoncalves <marthacristiny@Eds-Mac-mini.local>
2021-06-15 16:41:30 -0300

    Exercicio-e-desafio-do-dia

---
+++ semana1/operadores/index.js
@@ -0,0 +1,40 @@
+//------------ InterpretaçãoDeCodigo------------
+//------------Exercicio1------------
+
+/*1 - Resposta 
+
+a) false
+b) false
+c) true
+d) boolean
+*/
+
+//-----------------Exercicio2--------------
+
+/* O console ira retornar os numeros lado a lado, quase como uma concatenaçao. 
+se eu der dois numeros pra ele ele vai me retornar um numero do lado do outro!
+EX - com esse codigo: 1 + 1 = 11
+EX - com o codigo correto: 1+1 =2 */
+
+//------------------Exercicio3--------------
+
+//para solucionar o problema do amigo eu iria sugerir o seguinte codigo;
+
+const numero1 = Number (prompt("digite um numero!"))
+const numero2 = Number (prompt("digite um segundo numero!"))
+
+const soma = numero1 + numero2
+
+console.log(soma) 
+
+//--------------------ExerciciDeEscritaDeCodigo------------------
+
+//---------------------Exercicio1--------------------------------
+
+//----------------------Exercicio2-------------------------------
+
+//----------------------Exercicio3------------------------------
+
+//---------------------Exercicio4----------------------------
+
+//---------------------DESAFIO-------------------------------
