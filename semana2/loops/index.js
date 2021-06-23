a1802f8de9fd4b1a57701878c87da4fd8996eebc
MarthaCristinyAvelarGoncalves <marthacristiny@Eds-Mac-mini.local>
2021-06-23 16:57:28 -0300

    algumas-mudancas

---
+++ semana2/loops/index.js
@@ -0,0 +1,51 @@
+//Exercícios de interpretação de código ------------------------------>
+//Exercicio 1 -------------------------------------------------------->
+/*
+
+Resposta:
+ Esta criando um looping onde a condiçao se repete ate que o valor seja igual ou maior que 5.
+ acredito que no console deve aparecer 10 pois na condiçao estamos acresentando 1 a cada vez que 
+ reptimos a condição!
+ 
+ //Exercicio 2 -------------------------------------------------------->
+Resposta:
+
+a) Ira aparecer todos os numeros maiores que 18 ate o ultimo elemetno do array. 
+-----------
+b) Eu acho que nao porem estou com dificuldade de dizer qual seria melhor maneira de fazer isso
+
+ //Exercicio 3 -------------------------------------------------------->
+
+Resposta:
+ Aparecereiam 4 linhas; A primeira linha com 1 asterisco, a segunda com 2, a terceira com 3, a quarta com 4.
+ Estou em duvida sobre o final do programa onde tem quantidadeAtual++, nao sei apareceria mais uma linha por 
+ causa disso ou mais um linha com 5 asteriscos.
+
+*/
+ //Exercícios de escrita de código ------------------------------>
+//Exercicio 1 -------------------------------------------------------->
+
+quantidadeDeBicinhos = Number (prompt("Quantos bicinhos de estimação você tem?"))
+
+    if(quantidadeDeBicinhos === 0){
+        console.log("Que pena você pode adotar um pet!")
+    }  else if(quantidadeDeBicinhos === 4 ){
+            const quantosTem = prompt("Quais os nomes dos animais que você tem?")
+                const arrayDeBichinhos = 0
+            }
+                while(arrayDeBichinhos = 4){
+                arrayDeBichinhos += quantosTem
+           
+    }
+//Exercicio 2 -------------------------------------------------------->
+
+const arrayOriginal = [0, 1, 2, 3, 4, 5]
+
+const valores = (arrayOriginal) => {
+    const valores2 = arrayOriginal
+}
+//DESAFIO -------------------------------------------------------->
+
+
+
+
