c9877ebc192d5d02b3393186904834e9de1c68f8
MarthaCristiny <85270115+MarthaCristiny@users.noreply.github.com>
2021-08-04 19:38:50 -0300

    adicionando-conteudos3

---
+++ semana2/condicionais/index.js
@@ -0,0 +1,10 @@
+/*Interpretação de código
+1 - RESPOSTA:
+
+        a) - O codigo quer verificar se o numero fornecido pelo usuario é divisivel por 2, Caso o numero seja divisivel por 2 a primeira condiçao sera executada, caso contrario a segundo condição sera executada.
+        b) -  para numeros pares
+        b) -  para numeros impares
+
+2 - RESPOSTA:
+        a) - O codigo acima recebe o nome de uma fruta escolhida pelo o usuario, e entao verifica se tem essa fruta na lista, a lista informa alem de nome o preço, caso o usuario tenha escolhido uma fruta que nao tem na lista o codigo esta programado para responder automaticamente que esta fruta custa 5 reais. E o console ira imprimir o nome e preço da fruta. 
+*/
